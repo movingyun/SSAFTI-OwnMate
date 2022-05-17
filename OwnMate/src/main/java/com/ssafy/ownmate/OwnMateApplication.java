1d9c0c859d1e9ad478ff28ce3cc5166100c8f0ee
movingyun <ydy1107@naver.com>
2022-05-17 15:50:39 +0900

    github init

---
+++ OwnMate/src/main/java/com/ssafy/ownmate/OwnMateApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ownmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnMateApplication.class, args);
+	}
+
+}
---
+++ OwnMate/src/test/java/com/ssafy/ownmate/OwnMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ownmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
