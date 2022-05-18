db1d96940d92713ad3f52b846774bb045405e061
송유경 <syg9929@naver.com>
2022-05-18 13:21:46 +0900

    Follower dto 추가

---
+++ OwnMate/src/main/java/com/ssafy/ownmate/model/dto/Follower.java
@@ -0,0 +1,13 @@
+package com.ssafy.ownmate.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Follower {
+	private String userId;
+	private String targetId;
+}
