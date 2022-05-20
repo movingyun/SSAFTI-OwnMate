9129fa5d8bae40216f58c2cb0ec249cd518001ed
유동윤 <ydy1107@naver.com>
2022-05-20 21:02:45 +0900

    영상 조회수 순으로

--- OwnMate/src/main/java/com/ssafy/ownmate/model/service/ZzimService.java
+++ OwnMate/src/main/java/com/ssafy/ownmate/model/service/ZzimService.java
@@ -1,18 +1,18 @@
-package com.ssafy.ownmate.model.dao;
+package com.ssafy.ownmate.model.service;
 
 import java.util.List;
 
 import com.ssafy.ownmate.model.dto.Zzim;
 
-public interface zzimDao {
+public interface ZzimService {
 	//찜 목록
-	public List<Zzim> selectZzimList();
+	public List<Zzim> getZzimList();
 	
 	//유저가 찜한거 하나 조회
-	public Zzim selectZzimByUser(String userId);
+	public Zzim getZzimByUser(String userId);
 	
 	//찜 등록
-	public void insertZzim(Zzim zzim);
+	public void addZzim(Zzim zzim);
 	
 	//찜 삭제
 	public int deleteZzim(int zzimNo);
